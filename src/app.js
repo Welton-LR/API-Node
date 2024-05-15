8cecba604a92ca6450eac02239519ef38998f76a
Welton <weltonrdesigner@gmail.com>
2024-05-14 21:42:10 -0300

    separando as rotas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const express = require ('express');
-const app = express();
 const bodyParser = require('body-parser');
+
+const app = express();
 const router = express.Router();
-const index = require('./routes/index');
+
+// Carregar as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
 // Configurando o body-parser para interpretar solicitações JSON
 app.use(bodyParser.json());
@@ -12,25 +16,10 @@ app.use(bodyParser.json());
 // Configurando o body-parser para interpretar solicitações de formulário
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-})
 
-const put = router.put('/:id', (req, res, next) => {
-    const id =req.params.id;
-    res.status(200).send({
-        id : id,
-        item : req.body
-    });
-})
 
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-})
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
-app.use('/', index);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
 
 module.exports = app;
