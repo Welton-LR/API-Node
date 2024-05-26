b871a5a9dc58434c667be5e935b0faabbae7357a
Welton <weltonrdesigner@gmail.com>
2024-05-26 19:50:42 -0300

    status code

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -31,12 +31,12 @@ exports.post = (req, res, next) => {
 
 exports.put = (req, res, next) => {
     const id =req.params.id;
-    res.status(200).send({
+    res.status(201).send({
         id : id,
         item : req.body
     });
 };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    res.status(204).send(req.body);
 }
